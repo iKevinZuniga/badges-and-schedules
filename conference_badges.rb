3e8ec2072c400e1ac8f95153ccc42e2926d49489
iKevinZuniga <kevin@puresyncore.com>
2020-10-08 12:11:46 -0500

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(array)
+    new_array = []
+    array.each do |name|
+        new_array.push("Hello, my name is #{name}.")
+    end
+    return new_array
+end
+
+def assign_rooms(array)
+    nuarray = []
+    counter = 1
+    array.each do |name|
+        nuarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+        counter += 1
+    end
+    return nuarray
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |id|
+        puts id
+    end
+    assign_rooms(array).each do |id|
+        puts id
+    end
+end
+
+
+
+
